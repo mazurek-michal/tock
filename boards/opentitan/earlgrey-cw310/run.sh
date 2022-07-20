0e1f65f66b739149990bee1d47a94f2cf96abd61
Chris Frantz <cfrantz@google.com>
2022-07-20 12:09:50 -0700

    Update Makefile to query bazel in OPENTITAN_TREE
    Note: I've tested `make flash` and `make test-hardware`.  I have not
    tested any verilator or qemu flows.
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>

--- boards/opentitan/earlgrey-cw310/run.sh
+++ boards/opentitan/earlgrey-cw310/run.sh
@@ -18,6 +18,16 @@ if [[ "${VERILATOR}" == "yes" ]]; then
 			printf "\n[CW-130: Verilator Tests]: Setting up verilator_build...\n\n"
 			mkdir "$BUILD_DIR"
 		fi
+
+
+	${OPENTITAN_TREE}/bazelisk.sh build \
+		//hw:verilator \
+		//hw/ip/otp_ctrl/data:img_rma \
+		//sw/device/lib/testing/test_rom:test_rom
+	VERILATOR_DIR=$(${OPENTITAN_TREE}/bazelisk.sh outquery //hw:verilator)
+	OTP_IMAGE=$(${OPENTITAN_TREE}/bazelisk.sh outquery //hw/ip/otp_ctrl/data:img_rma)
+	TEST_ROM=$(${OPENTITAN_TREE}/bazelisk.sh outquery //sw/device/lib/testing/test_rom:test_rom_sim_verilator_scr_vmem)
+
 	# Copy in and covert from cargo test output to binary
 	${OBJCOPY} ${1} "$BUILD_DIR"/earlgrey-cw310-tests.elf
 	if [[ "${APP}" != "" ]]; then
@@ -31,15 +41,18 @@ if [[ "${VERILATOR}" == "yes" ]]; then
 		--binary --offset 0 --byte-swap 8 --fill 0xff \
 		-within "$BUILD_DIR"/earlgrey-cw310-tests.bin\
 		-binary -range-pad 8 --output "$BUILD_DIR"/binary.64.vmem --vmem 64
-	${OPENTITAN_TREE}/bazel-bin/hw/build.verilator_real/sim-verilator/Vchip_sim_tb \
-		--meminit=rom,${OPENTITAN_TREE}/bazel-out/k8-fastbuild-ST-2cc462681f62/bin/sw/device/lib/testing/test_rom/test_rom_sim_verilator.39.scr.vmem \
+	${OPENTITAN_TREE}/${VERILATOR_DIR}/sim-verilator/Vchip_sim_tb \
+		--meminit=rom,${OPENTITAN_TREE}/${TEST_ROM} \
 		--meminit=flash,./"$BUILD_DIR"/binary.64.vmem \
-		--meminit=otp,${OPENTITAN_TREE}/bazel-out/k8-fastbuild/bin/hw/ip/otp_ctrl/data/img_rma.24.vmem
+		--meminit=otp,${OPENTITAN_TREE}/${OTP_IMAGE}
 elif [[ "${OPENTITAN_TREE}" != "" ]]; then
 	${OBJCOPY} --update-section .apps=${APP} ${1} bundle.elf
 	${OBJCOPY} --output-target=binary bundle.elf binary
 
-	${OPENTITAN_TREE}/bazel-bin/sw/host/opentitantool/opentitantool.runfiles/lowrisc_opentitan/sw/host/opentitantool/opentitantool --interface=cw310 bootstrap binary
+	${OPENTITAN_TREE}/bazelisk.sh run //sw/host/opentitantool -- \
+        --rcfile= \
+		--interface=cw310 --conf=${OPENTITAN_TREE}/sw/host/opentitantool/config/opentitan_cw310.json \
+        bootstrap $(realpath binary)
 else
 	../../../tools/qemu/build/qemu-system-riscv32 -M opentitan -nographic -serial stdio -monitor none -semihosting -kernel "${1}" -global driver=riscv.lowrisc.ibex.soc,property=resetvec,value=${QEMU_ENTRY_POINT}
 fi
