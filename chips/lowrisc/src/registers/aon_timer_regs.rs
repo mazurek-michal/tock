260eec808aa875031f861041b1f13c70c46e9cae
Chris Frantz <cfrantz@google.com>
2022-07-20 14:03:42 -0700

    Autogenerate register definitions for lowrisc peripherals
    The register definitions were autogenerated from the OpenTitan repository.
    
    ```bash
    cd $OPENTITAN_REPO_DIR
    bazel build //sw/device/tock:tock_lowrisc_registers
    tar -C $TOCK_REPO_DIR -xf bazel-bin/sw/device/tock/tock_lowrisc_registers.tar
    ```
    
    This commit does not cause the drivers to use these definitions yet.
    Follow-up commits will update the drivers to use the autogenerated
    definitions rather than the current by-hand definitions.
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>

---
+++ chips/lowrisc/src/registers/aon_timer_regs.rs
@@ -0,0 +1,82 @@
+// Generated register constants for aon_timer.
+// This file is licensed under either of:
+//   Apache License, Version 2.0 (LICENSE-APACHE <http://www.apache.org/licenses/LICENSE-2.0>)
+//   MIT License (LICENSE-MIT <http://opensource.org/licenses/MIT>)
+
+// Built for earlgrey_silver_release_v5-6422-g05dcfbd00
+// https://github.com/lowRISC/opentitan/tree/05dcfbd00ca893dba034b468d1754f3f50780080
+// Tree status: clean
+// Build date: 2022-07-20T21:02:26
+
+// Original reference file: hw/ip/aon_timer/data/aon_timer.hjson
+use kernel::utilities::registers::ReadWrite;
+use kernel::utilities::registers::{register_bitfields, register_structs};
+// Number of alerts
+pub const AON_TIMER_PARAM_NUM_ALERTS: u32 = 1;
+// Register width
+pub const AON_TIMER_PARAM_REG_WIDTH: u32 = 32;
+
+register_structs! {
+    pub AonTimerRegisters {
+        // Alert Test Register
+        (0x0000 => pub(crate) alert_test: ReadWrite<u32, ALERT_TEST::Register>),
+        // Wakeup Timer Control register
+        (0x0004 => pub(crate) wkup_ctrl: ReadWrite<u32, WKUP_CTRL::Register>),
+        // Wakeup Timer Threshold Register
+        (0x0008 => pub(crate) wkup_thold: ReadWrite<u32, WKUP_THOLD::Register>),
+        // Wakeup Timer Count Register
+        (0x000c => pub(crate) wkup_count: ReadWrite<u32, WKUP_COUNT::Register>),
+        // Watchdog Timer Write Enable Register
+        (0x0010 => pub(crate) wdog_regwen: ReadWrite<u32, WDOG_REGWEN::Register>),
+        // Watchdog Timer Control register
+        (0x0014 => pub(crate) wdog_ctrl: ReadWrite<u32, WDOG_CTRL::Register>),
+        // Watchdog Timer Bark Threshold Register
+        (0x0018 => pub(crate) wdog_bark_thold: ReadWrite<u32, WDOG_BARK_THOLD::Register>),
+        // Watchdog Timer Bite Threshold Register
+        (0x001c => pub(crate) wdog_bite_thold: ReadWrite<u32, WDOG_BITE_THOLD::Register>),
+        // Watchdog Timer Count Register
+        (0x0020 => pub(crate) wdog_count: ReadWrite<u32, WDOG_COUNT::Register>),
+        // Interrupt State Register
+        (0x0024 => pub(crate) intr_state: ReadWrite<u32, INTR_STATE::Register>),
+        // Interrupt Test Register
+        (0x0028 => pub(crate) intr_test: ReadWrite<u32, INTR_TEST::Register>),
+        // Wakeup request status
+        (0x002c => pub(crate) wkup_cause: ReadWrite<u32, WKUP_CAUSE::Register>),
+        (0x0030 => @END),
+    }
+}
+
+register_bitfields![u32,
+    pub(crate) ALERT_TEST [
+        FATAL_FAULT OFFSET(0) NUMBITS(1) [],
+    ],
+    pub(crate) WKUP_CTRL [
+        ENABLE OFFSET(0) NUMBITS(1) [],
+        PRESCALER OFFSET(1) NUMBITS(12) [],
+    ],
+    pub(crate) WKUP_THOLD [],
+    pub(crate) WKUP_COUNT [],
+    pub(crate) WDOG_REGWEN [
+        REGWEN OFFSET(0) NUMBITS(1) [],
+    ],
+    pub(crate) WDOG_CTRL [
+        ENABLE OFFSET(0) NUMBITS(1) [],
+        PAUSE_IN_SLEEP OFFSET(1) NUMBITS(1) [],
+    ],
+    pub(crate) WDOG_BARK_THOLD [],
+    pub(crate) WDOG_BITE_THOLD [],
+    pub(crate) WDOG_COUNT [],
+    pub(crate) INTR_STATE [
+        WKUP_TIMER_EXPIRED OFFSET(0) NUMBITS(1) [],
+        WDOG_TIMER_BARK OFFSET(1) NUMBITS(1) [],
+    ],
+    pub(crate) INTR_TEST [
+        WKUP_TIMER_EXPIRED OFFSET(0) NUMBITS(1) [],
+        WDOG_TIMER_BARK OFFSET(1) NUMBITS(1) [],
+    ],
+    pub(crate) WKUP_CAUSE [
+        CAUSE OFFSET(0) NUMBITS(1) [],
+    ],
+];
+
+// End generated register constants for aon_timer
---
+++ chips/lowrisc/src/registers/clkmgr_regs.rs
@@ -0,0 +1,46 @@
+// Generated register constants for CLKMGR.
+// This file is licensed under either of:
+//   Apache License, Version 2.0 (LICENSE-APACHE <http://www.apache.org/licenses/LICENSE-2.0>)
+//   MIT License (LICENSE-MIT <http://opensource.org/licenses/MIT>)
+
+// Built for earlgrey_silver_release_v5-6422-g05dcfbd00
+// https://github.com/lowRISC/opentitan/tree/05dcfbd00ca893dba034b468d1754f3f50780080
+// Tree status: clean
+// Build date: 2022-07-20T21:02:26
+
+// Original reference file: hw/ip/clkmgr/data/clkmgr.hjson
+use kernel::utilities::registers::ReadWrite;
+use kernel::utilities::registers::{register_bitfields, register_structs};
+// Number of clock groups
+pub const CLKMGR_PARAM_NUM_GROUPS: u32 = 7;
+// Register width
+pub const CLKMGR_PARAM_REG_WIDTH: u32 = 32;
+
+register_structs! {
+    pub ClkmgrRegisters {
+        // Clock enable for software gateable clocks.
+        (0x0000 => pub(crate) clk_enables: ReadWrite<u32, CLK_ENABLES::Register>),
+        // Clock hint for software gateable clocks.
+        (0x0004 => pub(crate) clk_hints: ReadWrite<u32, CLK_HINTS::Register>),
+        // Since the final state of !!CLK_HINTS is not always determined by software,
+        (0x0008 => pub(crate) clk_hints_status: ReadWrite<u32, CLK_HINTS_STATUS::Register>),
+        (0x000c => @END),
+    }
+}
+
+register_bitfields![u32,
+    pub(crate) CLK_ENABLES [
+        CLK_FIXED_PERI_EN OFFSET(0) NUMBITS(1) [],
+        CLK_USB_48MHZ_PERI_EN OFFSET(1) NUMBITS(1) [],
+    ],
+    pub(crate) CLK_HINTS [
+        CLK_MAIN_AES_HINT OFFSET(0) NUMBITS(1) [],
+        CLK_MAIN_HMAC_HINT OFFSET(1) NUMBITS(1) [],
+    ],
+    pub(crate) CLK_HINTS_STATUS [
+        CLK_MAIN_AES_VAL OFFSET(0) NUMBITS(1) [],
+        CLK_MAIN_HMAC_VAL OFFSET(1) NUMBITS(1) [],
+    ],
+];
+
+// End generated register constants for CLKMGR
