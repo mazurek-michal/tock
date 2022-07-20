ff3c0b4339860b65c0846aa4c09429be8831e1e1
Chris Frantz <cfrantz@google.com>
2022-07-20 12:09:50 -0700

    Update Makefile to query bazel in OPENTITAN_TREE
    Note: I've tested `make flash` and `make test-hardware`.  I have not
    tested any verilator or qemu flows.
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>

--- boards/opentitan/earlgrey-cw310/run.sh
+++ boards/opentitan/earlgrey-cw310/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# If OBJCOPY isn't already set, use the one from OpenTitan's toolchain
+: ${OBJCOPY:=/tools/riscv/bin/riscv32-unknown-elf-objcopy}
+
 BUILD_DIR="verilator_build/"
 
 if [[ "${VERILATOR}" == "yes" ]]; then
@@ -11,6 +14,16 @@ if [[ "${VERILATOR}" == "yes" ]]; then
 			printf "\n[CW-130: Verilator Tests]: Setting up verilator_build...\n\n"
 			mkdir "$BUILD_DIR"
 		fi
+
+
+	${OPENTITAN_TREE}/bazelisk.sh build \
+		//hw:verilator \
+		//hw/ip/otp_ctrl/data:img_rma \
+		//sw/device/lib/testing/test_rom:test_rom
+	VERILATOR_DIR=$(${OPENTITAN_TREE}/bazelisk.sh outquery //hw:verilator)
+	OTP_IMAGE=$(${OPENTITAN_TREE}/bazelisk.sh outquery //hw/ip/otp_ctrl/data:img_rma)
+	TEST_ROM=$(${OPENTITAN_TREE}/bazelisk.sh outquery //sw/device/lib/testing/test_rom:test_rom_sim_verilator_scr_vmem)
+
 	# Copy in and covert from cargo test output to binary
 	${OBJCOPY} ${1} "$BUILD_DIR"/earlgrey-cw310-tests.elf
 	${OBJCOPY} --output-target=binary "$BUILD_DIR"/earlgrey-cw310-tests.elf "$BUILD_DIR"/earlgrey-cw310-tests.bin
@@ -19,22 +32,18 @@ if [[ "${VERILATOR}" == "yes" ]]; then
 		--binary --offset 0 --byte-swap 8 --fill 0xff \
 		-within "$BUILD_DIR"/earlgrey-cw310-tests.bin\
 		-binary -range-pad 8 --output "$BUILD_DIR"/binary.64.vmem --vmem 64
-	${OPENTITAN_TREE}/bazel-out/k8-fastbuild/bin/hw/build.verilator_real/sim-verilator/Vchip_sim_tb \
-		--meminit=rom,${OPENTITAN_TREE}/bazel-out/k8-fastbuild-ST-97f470ee3b14/bin/sw/device/lib/testing/test_rom/test_rom_sim_verilator.scr.39.vmem \
+	${OPENTITAN_TREE}/${VERILATOR_DIR}/sim-verilator/Vchip_sim_tb \
+		--meminit=rom,${OPENTITAN_TREE}/${TEST_ROM} \
 		--meminit=flash,./"$BUILD_DIR"/binary.64.vmem \
-		--meminit=otp,${OPENTITAN_TREE}/bazel-out/k8-fastbuild/bin/sw/device/tests/otp_ctrl_smoketest_sim_verilator.runfiles/lowrisc_opentitan/hw/ip/otp_ctrl/data/img_rma.vmem
+		--meminit=otp,${OPENTITAN_TREE}/${OTP_IMAGE}
 elif [[ "${OPENTITAN_TREE}" != "" ]]; then
-	riscv64-linux-gnu-objcopy --update-section .apps=${APP} ${1} bundle.elf
-	riscv64-linux-gnu-objcopy --output-target=binary bundle.elf binary
+	${OBJCOPY} --update-section .apps=${APP} ${1} bundle.elf
+	${OBJCOPY} --output-target=binary bundle.elf binary
 
-	${OPENTITAN_TREE}/build-out/sw/host/rom_ext_signer \
-	rom_ext \
-	binary \
-	${OPENTITAN_TREE}/sw/device/silicon_creator/mask_rom/keys/test_key_0_rsa_3072_exp_f4.der \
-	bundle.elf \
-	binary.signed
-
-	${OPENTITAN_TREE}/util/fpga/cw310_loader.py --firmware binary.signed
+	${OPENTITAN_TREE}/bazelisk.sh run //sw/host/opentitantool -- \
+        --rcfile= \
+		--interface=cw310 --conf=${OPENTITAN_TREE}/sw/host/opentitantool/config/opentitan_cw310.json \
+        bootstrap $(realpath binary)
 else
 	../../../tools/qemu/build/qemu-system-riscv32 -M opentitan -bios ../../../tools/qemu-runner/opentitan-boot-rom.elf -nographic -serial stdio -monitor none -semihosting -kernel "${1}"
 fi
